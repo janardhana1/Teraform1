0bc6aeec0557369ad6f08061dc44ea8a3567867a
Sriharsha Kalluru <skalluru@mac.local>
2021-11-08 07:44:01 +0530

    Update terraform/

--- examples/ec2/main.tf
+++ examples/ec2/main.tf
@@ -1,9 +1,11 @@
 resource "aws_instance" "sample" {
   ami                    = "ami-0dc863062bc04e1de"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = []
+  vpc_security_group_ids = [var.SGID]
 
   tags = {
     Name = "sample"
   }
 }
+
+variable "SGID" {}
